b5a62fe8da6e34e82e13c1ff9513699b5bca4c4a
julauren <249057325+Simeriash@users.noreply.github.com>
2026-01-24 10:47:54 +0100

    input error management

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 10:02:03 by julauren          #+#    #+#             */
-/*   Updated: 2026/01/24 06:31:12 by julauren         ###   ########.fr       */
+/*   Updated: 2026/01/24 08:25:06 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define SO_LONG_H
 
 # include "minilibx-linux/mlx.h"
+# include "src/Libft/libft.h"
 # include <stdlib.h>
 # include <fcntl.h>
+# include <unistd.h>
 
 #endif
