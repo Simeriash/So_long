7ef37f2c11dd7e4eda8f75adb12367a8f57887e6
julauren <249057325+Simeriash@users.noreply.github.com>
2026-01-24 06:52:57 +0100

    add map + libft

---
+++ so_long.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/23 10:02:03 by julauren          #+#    #+#             */
+/*   Updated: 2026/01/24 06:31:12 by julauren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "minilibx-linux/mlx.h"
+# include <stdlib.h>
+# include <fcntl.h>
+
+#endif
